d3a98a0a87f5cc3752c09c1d1f50388da23b6ae8
WRXer <king_311@mail.ru>
2023-03-09 09:38:44 +0400

    Исправлено обращение к ключу Написана функция:  -получения айди  -вывода на консоль инфо

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import json
+import os
+from googleapiclient.discovery import build
+import isodate
 
 
 class Channel:
@@ -5,8 +9,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.api_key: str = os.getenv('API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.dict_to_print = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.dict_to_print, indent=2, ensure_ascii=False))
