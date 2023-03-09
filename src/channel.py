8e3421a2b04c4b4b7f6b65d5bbfd285674146747
Serg Ionov <s3rgvx@gmail.com>
2023-03-10 00:13:12 +0500

    add utils.py and add code in channel.py, add requirements.txt

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+from src.utils import print_inf
 
 
 class Channel:
@@ -5,8 +6,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
         pass
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+        print_inf(self.channel_id)
         pass
---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+
+def print_inf(channel_id):
+    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+    printj(channel)
