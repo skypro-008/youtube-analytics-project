a5a474764023896e17011b196dc46cfc3fb27e79
WRXer <king_311@mail.ru>
2023-03-12 13:19:32 +0400

    Модифицировал конструктор `Channel`, чтобы после инициализации экземпляр имел следующие атрибуты, заполненные реальными данными канала: - id канала - название канала - описание канала - ссылка на канал - количество подписчиков - количество видео - общее количество просмотров
    Добавил в класс `Channel` следующие методы:
    - класс-метод `get_service()`, возвращающий объект для работы с YouTube API
    - метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра `Channel`

--- homework-2/main.py
+++ homework-2/main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
 
     # менять не можем
-    vdud.channel_id = 'Новое название'
-    # AttributeError: property 'channel_id' of 'Channel' object has no setter
+    ##vdud.channel_id = 'Новое название'
+    #AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
--- src/channel.py
+++ src/channel.py
@@ -1,20 +1,50 @@
 import json
 import os
 from googleapiclient.discovery import build
-import isodate
+
 
 
 class Channel:
     """Класс для ютуб-канала"""
-
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.api_key: str = os.getenv('API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        self.dict_to_print = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        #self.api_key: str = os.getenv('API_KEY')
+        #self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.dict_to_print = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]['description']
+        self.url = f"https://www.youtube.com/channel/{self.channel['items'][0]['id']}"
+        self.subscriber_count = self.channel["items"][0]['statistics']['subscriberCount']
+        self.video_count = self.channel["items"][0]['statistics']['videoCount']
+        self.view_count = self.channel["items"][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.dict_to_print, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, id):
+        raise AttributeError("Property 'channel_id' of 'Channel' object has no setter")
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        return cls.youtube
+
+    def to_json(self, path):
+        """
+        Функция, сохраняющая в файл значения атрибутов экземпляра channel
+        """
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(self.__dict__, file, ensure_ascii=False)
+
