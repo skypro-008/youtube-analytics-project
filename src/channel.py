1f39777dc58ef942f1631fa22442c508a8f85390
Ilia Perfilov <ind.climb@gmail.com>
2023-03-16 18:11:09 +0300

    chanel.py was updated - new methods added

--- src/channel.py
+++ src/channel.py
@@ -12,11 +12,36 @@ class Channel:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/'+ self.channel['items'][0]['id']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, file_name):
+        data = {
+            'ID Chanel': self.__channel_id,
+            'Name': self.title,
+            'Description': self.description,
+            'URL': self.url,
+            'Count of subscribers': self.subscriber_count,
+            'Count of videos': self.video_count,
+            'Count of views': self.view_count,
+        }
+        with open(file_name, 'w', encoding='utf-8') as outfile:
+            outfile.write(json.dumps(data, ensure_ascii=False))
+
 
