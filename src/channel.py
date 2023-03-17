86e5d4007a979012bf231f565adb51b0a225d6e1
Ilia Perfilov <ind.climb@gmail.com>
2023-03-17 16:15:03 +0300

    chanel.py was updated - added new magic methods (str, add, sub, le, ge)

--- src/channel.py
+++ src/channel.py
@@ -17,11 +17,10 @@ class Channel:
         self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
-        self.url = 'https://www.youtube.com/channel/'+ self.channel['items'][0]['id']
+        self.url = 'https://www.youtube.com/channel/' + self.channel['items'][0]['id']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
         self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -32,6 +31,7 @@ class Channel:
         return cls.youtube
 
     def to_json(self, file_name):
+        """Сохраняет информацию о канале в формате json"""
         data = {
             'ID Chanel': self.__channel_id,
             'Name': self.title,
@@ -44,4 +44,17 @@ class Channel:
         with open(file_name, 'w', encoding='utf-8') as outfile:
             outfile.write(json.dumps(data, ensure_ascii=False))
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
 
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
