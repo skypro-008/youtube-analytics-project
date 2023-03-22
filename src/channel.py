a53f8b7e910685ad80a7cbe34d422e860fb54523
Xena Ship <shipxena@gmail.com>
2023-03-22 11:22:19 +0300

    fixed

--- src/channel.py
+++ src/channel.py
@@ -10,9 +10,34 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https:////www.youtube.com//channel//' + self.channel['items'][0]['id']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+
+    def to_json(self, file_name):
+        with open(file_name, 'w', encoding='cp1251') as fh:
+            json.dump({
+                'channel_id': self.__channel_id,
+                'title': self.title,
+                'description': self.description,
+                'url': self.url,
+                'subscriber_count': self.subscriber_count,
+                'video_count': self.video_count,
+                'view_count': self.view_count
+            }, fh)
