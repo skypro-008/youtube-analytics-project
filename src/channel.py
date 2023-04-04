7c032338a787ed7ea5dbf4508e690239423cb406
Alex <santes.au@gmail.com>
2023-04-04 09:45:05 +0300

    1

--- src/channel.py
+++ src/channel.py
@@ -3,10 +3,10 @@
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.channel_id)
