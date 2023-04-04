8112fc2000623c19fa34b6b54c8250163862a594
Max <maks_orlov_2015@mail.ru>
2023-04-04 20:29:35 +1000

    задание 1

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -9,7 +9,6 @@ import isodate
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
-
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
 
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,17 @@
+import json
+import os
 
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
