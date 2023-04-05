584c42fefe5bfa0027e8b064e09af1ce918cec1a
Alex <santes.au@gmail.com>
2023-04-05 13:28:20 +0300

    1

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,7 @@
 import json
 import os
 import isodate
-from googleapiclient.discovery import discovery
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -12,10 +12,12 @@ class Channel:
         self.channel_id = channel_id
 
     def print_info(self):
+        """вывод информации о канале"""
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
         api_key: str = os.getenv('API_KEY')
-
-        # создать специальный объект для работы с API
         youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        print(self.channel_id)
+
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
