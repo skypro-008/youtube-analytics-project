200ff17eb541613a3994e421aad263e147c879ae
Sandi_Navoiy <santes.au@gmail.com>
2023-04-07 16:06:05 +0300

    Реализация 2

--- src/channel.py
+++ src/channel.py
@@ -12,8 +12,18 @@ class Channel:
         self.channel_id = channel_id
 
     def print_info(self):
-        """вывод информации о канале"""
+        """Вывод информации о канале"""
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+
+    def get_service(self):
+        """Возвращает объект для работы с YouTube API"""
+        pass
+
+
+    def to_json(self):
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        pass
