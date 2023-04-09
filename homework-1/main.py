947d54f70fe51460699a8d244b6334e9b534199b
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-04-09 12:33:28 +0300

    final

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,7 +2,7 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    vdud.print_info()
+    vdud.print_info(Channel.channel)
 
     """
 {
