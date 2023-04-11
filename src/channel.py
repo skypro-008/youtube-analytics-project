f9dac4178ee3bca8cc748b4da24415ed951109dd
davyd <davydow.val@yandex.ru>
2023-04-11 23:27:18 +0700

    A class for a YouTube channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+from helper.youtube_api_manual import youtube, printj
 
 
 class Channel:
@@ -5,8 +6,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = self.channel_id  # Редакция
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
