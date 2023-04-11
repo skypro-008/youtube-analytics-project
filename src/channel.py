a1b15c18b0975be0f4bf54f2ec0eecc71015b07a
d.larin <larin.dn@icloud.com>
2023-04-11 17:48:38 +0400

    homework-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = self.channel_id
+        youtube = self.youtube
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        return channel
