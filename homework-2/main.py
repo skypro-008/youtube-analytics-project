3bc9c9ba332979ed2b69602fe6d59ef003a86b0d
sviridov-andrey <chosenfew@yandex.ru>
2023-04-15 10:56:49 +0300

    _

--- homework-2/main.py
+++ homework-2/main.py
@@ -3,23 +3,18 @@ from src.channel import Channel
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 
-    print(vdud)
-
     # получаем значения атрибутов
     print(vdud.title)  # вДудь
     print(vdud.video_count)  # 163 (может уже больше)
-   # print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
-
+    print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
 
     # менять не можем
     vdud.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
-    print(vdud)
-
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
-    # # создаем файл 'vdud.json' в данными по каналу
-    # vdud.to_json('vdud.json')
+    # создаем файл 'vdud.json' в данными по каналу
+    vdud.to_json('vdud.json')
