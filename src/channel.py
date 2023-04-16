89aea2ad31e1c50d62ddb15fb6622290c300dac8
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-04-16 23:08:25 +0300

    final

--- src/channel.py
+++ src/channel.py
@@ -5,15 +5,11 @@ import os
 from googleapiclient.discovery import build
 
 #import isodate
-
-from helper.youtube_api_manual import youtube
-
-
+#from helper.youtube_api_manual import youtube
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
@@ -21,16 +17,43 @@ class Channel:
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.subscribers = self.channel["items"][0]["statistics"]["subscriberCount"]
+        print(f'''{self.channel_id} 
+        {self.channel}
+        {self.title}
+        {self.url}
+        {self.video_count}
+        {self.subscribers}''')
 
     def __str__(self):
-        return f"{self.channel_id}"
+        return f"{self.title} {(self.url)}"
 
-    def print_info(self, dict_to_print: dict) -> None:
+    def print_info(self, channel: dict) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     def get_service(cls):
         return cls.youtube
 
     def to_json(self, __init__):
         return json.dumps(__init__, indent=2, ensure_ascii=False)
+
+    def __add__(self, other):
+        return int(self.subscribers) + int(other.subscribers)
+
+    def __sub__(self, other):
+        return int(other.subscribers) - int(self.subscribers)
+
+    def __sub__(self, other):
+        return int(self.subscribers) - int(other.subscribers)
+
+    def __lt__(self, other):
+        return int(self.subscribers) < int(other.subscribers)
+    def __le__(self, other):
+        return int(self.subscribers) <= int(other.subscribers)
+
+    def __gt__(self, other):
+        return int(self.subscribers) > int(other.subscribers)
+
+    def __ge__(self, other):
+        return int(self.subscribers) >= int(other.subscribers)
