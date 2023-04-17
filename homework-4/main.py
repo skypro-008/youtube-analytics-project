128a6ea743edf697ad8897e9f692a5a2016027e8
Sandi_Navoiy <santes.au@gmail.com>
2023-04-17 22:31:25 +0300

    Реализация домашнего задания 4. Готово. Очень легко

--- homework-4/main.py
+++ homework-4/main.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     # Создаем два экземпляра класса
     video1 = Video('9lO06Zxhu88')  # '9lO06Zxhu88' - это id видео из ютуб
     video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+
     assert str(video1) == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
     assert str(video2) == 'Пушкин: наше все?'
--- src/video.py
+++ src/video.py
@@ -1,12 +1,32 @@
+import os
+
 from src.channel import Channel
+from googleapiclient.discovery import build
 
 
-class Video(Channel):
-    def __init__(self):
-        pass
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+        api_key: str = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                    id=video_id
+                                                    ).execute()
+        self.video_title: str = video_response['items'][0]['snippet']['title']  # название видео
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']  # количество просмотров
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']  # количество лайков
+        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']  # количество комментариев
+        self.url_video = f"https://www.youtube.com/channel/{self.video_id}"  # адрес видео
+
+    def __str__(self):
+        return f"{self.video_title}"
 
 
-
-class PLVideo(Channel):
-    def __init__(self):
-        pass
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id: str):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                 part='contentDetails',
+                                                                 maxResults=50,
+                                                                 ).execute()
