aa7f92ea6fa7418dc757dc82259c4a1202445978
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-04-22 17:05:17 +0300

    stable version of homework #2

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+import pickle
+import json
+
 from googleapiclient.discovery import build
 
 import os
@@ -10,11 +13,29 @@ class Channel:
     youtube = build('youtube', 'v3', developerKey=api_key)
     channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
 
+    #
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriberCount = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.viewCount = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(f'Информацию о канале{self.channel}')
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return cls.youtube
+
+    def to_json(self, dict_to_print: dict) -> None:
+        """Сохраняющий в файл значения атрибутов экземпляра `Channel`"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
