c35fc6b4d7a5682d7ac95908f6e4d57c3474138f
d.larin <larin.dn@icloud.com>
2023-04-27 23:54:38 +0400

    homework-3

--- src/channel.py
+++ src/channel.py
@@ -25,6 +25,57 @@ class Channel:
         self.video_count = int(self.channel["items"][0]["statistics"]["videoCount"])
         self.viewCount = int(self.channel["items"][0]["statistics"]["viewCount"])
 
+    def __str__(self):
+        """Магический метод для отображения информации об объекте класса для пользователя"""
+
+        return f'{self.title} {self.url}'
+
+    def __add__(self, other):
+        """Магический метод для складывания каналов по количеству подписчиков"""
+
+        if isinstance(other, self.__class__):
+            return self.subscriberCount + other.subscriberCount
+        else:
+            return self.subscriberCount + other
+
+    def __sub__(self, other):
+        """Магический метод для вычисления разницы каналов по количеству подписчиков"""
+
+        if isinstance(other, self.__class__):
+            return self.subscriberCount - other.subscriberCount
+        else:
+            return self.subscriberCount - other
+
+    def __ge__(self, other):
+        """Магический метод для сравнения каналов (больше или равно) по количеству подписчиков"""
+
+        if self.subscriberCount >= other.subscriberCount:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        """Магический метод для сравнения каналов (меньше или равно) по количеству подписчиков"""
+
+        if self.subscriberCount <= other.subscriberCount:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        """Магический метод для сравнения количества подписчиков (больше)"""
+        if self.subscriberCount > other.subscriberCount:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        """Магический метод для сравнения количества подписчиков (меньше)"""
+        if self.subscriberCount < other.subscriberCount:
+            return True
+        else:
+            return False
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
