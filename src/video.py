fc6406268be335b43ea515c181fdd3aed534f930
sviridov-andrey <chosenfew@yandex.ru>
2023-05-01 22:02:16 +0300

    Домашка 16_1

--- src/video.py
+++ src/video.py
@@ -1,9 +1,14 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# class HTTPErrors(Exception):
+#     def __init__(self, *args, **kwargs):
+#         self.message = args[0] if args else '000'
 
 
 class Video:
-
     api_key: str = os.getenv("YT_API_KEY")
     youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -11,10 +16,16 @@ class Video:
         self.video_id = video_id
         self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                          id=video_id).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.url_video = f"https://youtu.be/{self.video_id}"
-        self.viewCount = self.video_response['items'][0]['statistics']['viewCount']
-        self.likeCount = self.video_response['items'][0]['statistics']['likeCount']
+        if len(self.video_response['items']) == 0:
+            self.title = None
+            self.url_video = None
+            self.viewCount = None
+            self.like_count = None
+        else:
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url_video = f"https://youtu.be/{self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.title}'
