e5daf75b9df31aea87eb6a87826d4af4f8c15acd
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-05-02 12:59:06 +0300

    final

--- src/video.py
+++ src/video.py
@@ -1,21 +1,35 @@
 import os
-import json
+from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
-
-
+from urllib.request import urlopen
+from urllib.error import HTTPError
 class Video:
     def __init__(self, video_id: str) -> None:
         self.video_id: str = video_id
+        self.url = f'https://youtu.be/{self.video_id}'
+        try:
+            urlopen(self.url)
+        except HTTPError as err:
+            if err.code == 404:
+                print('Несуществующий url')
         self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                        id=video_id).execute()
-        self.title: str = self.video['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.video_id}'
-        self.view_count: int = self.video['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video['items'][0]['statistics']['commentCount']
-        self.video_count: int = self.video['items'][0]['statistics']['viewCount']
+        try:
+            self.title: str = self.video['items'][0]['snippet']['title']
+        except (ValueError,IndexError):
+            print(' Ошибка')
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.video_count = None
+            self.title: str = None
+        else:
+            self.view_count: int = self.video['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video['items'][0]['statistics']['commentCount']
+            self.video_count: int = self.video['items'][0]['statistics']['viewCount']
     def __str__(self):
         return f'{self.title}'
 
