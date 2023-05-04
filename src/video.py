2a42fb92cc081eef568e3e97a967ac64db4865df
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-05-04 13:14:28 +0300

    final

--- src/video.py
+++ src/video.py
@@ -6,30 +6,28 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 from urllib.request import urlopen
 from urllib.error import HTTPError
 class Video:
-    def __init__(self, video_id: str) -> None:
-        self.video_id: str = video_id
-        self.url = f'https://youtu.be/{self.video_id}'
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    title = None
+    url = None
+    views_count = None
+    like_count = None
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+
         try:
-            urlopen(self.url)
-        except HTTPError as err:
-            if err.code == 404:
-                print('Несуществующий url')
-        self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                       id=video_id).execute()
-        try:
-            self.title: str = self.video['items'][0]['snippet']['title']
-        except (ValueError,IndexError):
-            print(' Ошибка')
-            self.view_count = None
-            self.like_count = None
-            self.comment_count = None
-            self.video_count = None
-            self.title: str = None
-        else:
-            self.view_count: int = self.video['items'][0]['statistics']['viewCount']
-            self.like_count: int = self.video['items'][0]['statistics']['likeCount']
-            self.comment_count: int = self.video['items'][0]['statistics']['commentCount']
-            self.video_count: int = self.video['items'][0]['statistics']['viewCount']
+            self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                              id=self.video_id).execute()
+
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
+            self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except (IndexError, HTTPError, IndexError):
+            print('Video_id некорректен')
+
     def __str__(self):
         return f'{self.title}'
 
