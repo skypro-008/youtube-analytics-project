eb72c7438211a428d1cecc5f9621929e7088a605
Dante <dante1201@mail.ru>
2023-05-05 15:09:21 +0300

    Доработан класс Channel, добавлены новые методы, создан документ vdud.json в процессе проверки работы програмы

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 import os
 from googleapiclient.discovery import build
-import isodate
+import json
+
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -10,7 +11,22 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.__channel_id
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.channel)
+
+    def to_json(self, file_name):
+        with open(file_name, 'a', encoding="utf-8") as file:
+            json.dump(self.channel, file)
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
