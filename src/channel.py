b9a072577edd583d1d0bda61eb167fc198de773a
MaxSlidzen <slidzon94@gmail.com>
2023-05-13 15:26:00 +0300

    changed channel.py, added requirements

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+API_KEY: str = os.getenv('YT_API_KEY')
+YOUTUBE = build('youtube', 'v3', developerKey=API_KEY)
 
 
 class Channel:
@@ -5,8 +11,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.channel = YOUTUBE.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
