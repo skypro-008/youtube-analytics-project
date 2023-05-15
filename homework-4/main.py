f69ea46f6cabb16c858a0186cd4c992c1c4d4592
kirillskyeng <kirill.netreba@skyeng.ru>
2023-05-15 11:08:00 +0300

    Change sample channels

--- homework-4/main.py
+++ homework-4/main.py
@@ -2,7 +2,7 @@ from src.video import Video, PLVideo
 
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    video1 = Video('9lO06Zxhu88')  # '9lO06Zxhu88' - это id видео из ютуб
-    video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-    assert str(video1) == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
-    assert str(video2) == 'Пушкин: наше все?'
+    video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
+    video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
+    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
