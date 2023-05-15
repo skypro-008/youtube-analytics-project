9c9b526cc00830b45fcfe36f50b65d45bc18dc28
kirillskyeng <kirill.netreba@skyeng.ru>
2023-05-15 11:08:00 +0300

    Change sample channels

--- homework-3/main.py
+++ homework-3/main.py
@@ -2,17 +2,17 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    redactsiya = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    highload = Channel('UCwHL6WHUarjGfUM_586me8w')
 
     # Используем различные магические методы
-    print(vdud)  # 'вДудь (https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA)'
-    print(vdud + redactsiya)  # 13970000
-    print(vdud - redactsiya)  # 6630000
-    print(redactsiya - vdud)  # -6630000
-    print(vdud > redactsiya)  # True
-    print(vdud >= redactsiya)  # True
-    print(vdud < redactsiya)  # False
-    print(vdud <= redactsiya)  # False
-    print(vdud == redactsiya)  # False
+    print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
+    print(moscowpython + highload)  # 100100
+    print(moscowpython - highload)  # -48300
+    print(highload - moscowpython)  # 48300
+    print(moscowpython > highload)  # False
+    print(moscowpython >= highload)  # False
+    print(moscowpython < highload)  # True
+    print(moscowpython <= highload)  # True
+    print(moscowpython == highload)  # False
     
