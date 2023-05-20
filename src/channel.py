f215e52806cfd55e27765b37d069ad78d7ceb943
Polexa84 <polexa84@gmail.com>
2023-05-20 15:15:10 +0300

    Добавил необходимые методы функции

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(
+            json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute(), indent=2,
+                       ensure_ascii=False))
