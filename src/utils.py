e548d3cf8153a81dc69c95ce52e4fb6eb3dac4da
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-23 18:02:27 +0200

    changed find_value (more universal) (#8)

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,26 @@
-def find_value(dictionary: dict, aim: str) -> str:
-    """Находит и возвращает значение во вложенном словаре через рекурсию"""
-    
-    for key, value in dictionary.items():
-        if key == aim:
-            return value
-        elif isinstance(value, dict):
-            result = find_value(value, aim)
-            if result:
-                return result
-        elif isinstance(value, list):
-            for el in value:
-                if isinstance(el, (dict, list)):
+def find_value(collection, aim: str) -> str | None:
+    """
+    Находит и возвращает значение во вложенных коллекциях
+    (словари, списки, кортежи, множества) через рекурсию
+    """
+
+    if isinstance(collection, dict):
+
+        for key, value in collection.items():
+            if key == aim:
+                return value
+            elif isinstance(value, (dict, list, tuple, set)):
+                result = find_value(value, aim)
+                if result:
+                    return result
+    else:
+        try:
+            for el in collection:
+                if el == aim:
+                    return el
+                elif isinstance(el, (dict, list, tuple, set)):
                     result = find_value(el, aim)
                     if result:
                         return result
+        except TypeError:
+            return None
