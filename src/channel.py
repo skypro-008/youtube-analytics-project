56c76cd0e6920e5d47f1eb3686769af8b2f520cb
Polexa84 <polexa84@gmail.com>
2023-05-25 01:23:44 +0300

    Модифицировал конструктор Channel, чтобы после инициализации экземпляр имел следующие атрибуты, заполненные реальными данными канала:
        id канала
        название канала
        описание канала
        ссылка на канал
        количество подписчиков
        количество видео
        общее количество просмотров
    
    Добавил в класс Channel следующие методы:
    
        класс-метод get_service(), возвращающий объект для работы с YouTube API
        метод to_json(), сохраняющий в файл значения атрибутов экземпляра Channel

--- src/channel.py
+++ src/channel.py
@@ -9,12 +9,37 @@ class Channel:
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращающий объект для работы с YouTube API"""
+        return cls.youtube
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = Channel.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        # новые атрибуты
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
+
+        @property
+        def channel_id(self):
+            """Геттер для приватного атрибута channel_id"""
+            return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(
             json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute(), indent=2,
                        ensure_ascii=False))
+
+    def to_json(self, file_name):
+        yt_dict = {"id": self.channel_id, "title": self.title, "description": self.description, "url": self.url,
+                   "subscriber_count": self.subscriber_count, "video_count": self.video_count,
+                   "view_count": self.view_count}
+        with open(file_name, 'w', encoding="UTF-8") as file:
+            json.dump(yt_dict, file, indent=2, ensure_ascii=False)
