6eed4b79bb7c06563da19a01adfc98dffcbf816d
Yaroslav Galiulin <yaroslav66@list.ru>
2023-05-25 20:45:26 +0500

    Добавление магических методов

--- src/channel.py
+++ src/channel.py
@@ -7,6 +7,7 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     dict_hw_2 = {}
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
@@ -19,9 +20,33 @@ class Channel:
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.viewCount = channel['items'][0]['statistics']['viewCount']
 
+    def __str__(self):
+        return f'{self.title}, {self.url}'
+
+    def __add__(self, other):
+        return int(self.subscriberCount) + int(other.subscriberCount)
+
+    def __sub__(self, other):
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __gt__(self, other):
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __lt__(self, other):
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        return int(self.subscriberCount) == int(other.subscriberCount)
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = Channel.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         channel_info = json.dumps(channel, indent=2, ensure_ascii=False)
         print(channel_info)
 
@@ -35,7 +60,7 @@ class Channel:
     def to_json(self, file_name):
         """Мохраняющий в файл значения атрибутов экземпляра `Channel`"""
         # dict_hw_2 = self.dict_hw_2
-        self.dict_hw_2['id'] = self.channel_id
+        self.dict_hw_2['id'] = self.__channel_id
         self.dict_hw_2['title'] = self.title
         self.dict_hw_2['description'] = self.description
         self.dict_hw_2['url'] = self.url
@@ -45,14 +70,3 @@ class Channel:
 
         with open(file_name, 'w', encoding='utf-8') as f:
             json.dump(self.dict_hw_2, f, indent=2, ensure_ascii=False)
-
-
-
-
-
-
-
-
-
-
-
