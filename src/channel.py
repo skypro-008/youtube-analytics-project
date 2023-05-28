85ea42aeecac30335e563cc0a2c5e96aa5f43ab6
Kirill Aliyev <kirill.linas@gmail.com>
2023-05-28 15:43:01 +0400

    homework

--- src/channel.py
+++ src/channel.py
@@ -1,170 +1,138 @@
 import json
 import os
-from googleapiclient.discovery import build
-from googleapiclient.discovery import Resource
+
 from src.utils import find_value
+from pprint import pprint
+from googleapiclient.discovery import build
 from functools import total_ordering
 
 
 @total_ordering
 class Channel:
     """Класс для ютуб-канала"""
-
-    service = "youtube"
-    version = "v3"
-    name_key = "API_KEY"
+    service_name: str = 'youtube'
+    service_version: str = 'v3'
+    yt_api_key: str = os.getenv('YT_API_KEY')
+    service = build(service_name, service_version, developerKey=yt_api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется по id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id: str = channel_id
+        self.__channel_name = None
+        self.__channel_description = None
+        self.__channel_url = None
+        self.__channel_subscribers_count = None
+        self.__channel_video_count = None
+        self.__channel_summary_views = None
 
-        self.__channel_id = channel_id
-        self.__title = None
-        self.__description = None
-        self.__url = None
-        self.__subscribers_count = None
-        self.__video_count = None
-        self.__views_count = None
+        self.channel = Channel.service.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
-        self.set_atr()
+        self.set_info()
+
+    def set_info(self):
+        self.__channel_name: str = find_value(self.channel, 'title')
+        self.__channel_description: str = find_value(self.channel, 'description')
+        self.__channel_url: str = f"https://www.youtube.com/channel/{find_value(self.channel, 'id')}"
+        self.__channel_subscribers_count: int = int(find_value(self.channel, 'subscriberCount'))
+        self.__channel_video_count: int = int(find_value(self.channel, 'videoCount'))
+        self.__channel_summary_views: int = int(find_value(self.channel, 'viewCount'))
+
+    def __str__(self):
+        return f"{self.channel_name} ({self.__channel_url})"
+
+    def __add__(self, other):
+        try:
+            return self.channel_subscribers_count + other.channel_subscribers_count
+        except AttributeError:
+            return 'Сравниваемый атрибут отсутствует'
+
+    def __sub__(self, other):
+        try:
+            return self.channel_subscribers_count - other.channel_subscribers_count
+        except AttributeError:
+            return 'Сравниваемый атрибут отсутствует'
+
+    def __eq__(self, other):
+        try:
+            return self.channel_subscribers_count == other.channel_subscribers_count
+        except AttributeError:
+            return 'Сравниваемый атрибут отсутствует'
+
+    def __lt__(self, other):
+        try:
+            return self.channel_subscribers_count < other.channel_subscribers_count
+        except AttributeError:
+            return 'Сравниваемый атрибут отсутствует'
 
     @property
     def channel_id(self) -> str:
         return self.__channel_id
 
     @property
-    def title(self) -> str:
-        return self.__title
+    def channel_name(self) -> str:
+        return self.__channel_name
+
+    @channel_name.setter
+    def channel_name(self, channel_name: str):
+        self.__channel_name = channel_name
 
     @property
-    def description(self) -> str:
-        return self.__description
+    def channel_description(self) -> str:
+        return self.__channel_description
+
+    @channel_description.setter
+    def channel_description(self, channel_description: str):
+        self.__channel_description = channel_description
 
     @property
-    def url(self) -> str:
-        return self.__url
+    def channel_url(self) -> str:
+        return self.__channel_url
+
+    @channel_url.setter
+    def channel_url(self, channel_url: str):
+        self.__channel_url = channel_url
 
     @property
-    def subscribers_count(self) -> str:
-        return self.__subscribers_count
+    def channel_subscribers_count(self) -> int:
+        return self.__channel_subscribers_count
+
+    @channel_subscribers_count.setter
+    def channel_subscribers_count(self, channel_subscribers_count: int):
+        self.__channel_subscribers_count = channel_subscribers_count
 
     @property
-    def video_count(self) -> str:
-        return self.__video_count
+    def channel_video_count(self) -> int:
+        return self.__channel_video_count
+
+    @channel_video_count.setter
+    def channel_video_count(self, channel_video_count: int):
+        self.channel_video_count = channel_video_count
 
     @property
-    def views_count(self) -> str:
-        return self.__views_count
+    def channel_summary_views(self) -> int:
+        return self.__channel_summary_views
 
-    def __str__(self) -> str:
-        """Возвращает строку в формате: `<название_канала> (<ссылка_на_канал>)`"""
-
-        return f"{self.title} ({self.url})"
-
-    def get_info(self) -> dict:
-        """Получает данные о канале по его id"""
-
-        manager = self.get_service()
-
-        channel = manager.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return channel
-
-    def set_atr(self) -> None:
-        """
-        Устанавливает значения основных атрибутов объекта
-        на основании полученных по id данных канала
-        """
-
-        channel = self.get_info()
-
-        self.__title = find_value(channel, "title")
-        self.__description = find_value(channel, "description")
-        self.__url = f'https://www.youtube.com/channel/{find_value(channel, "id")}'
-        self.__subscribers_count = find_value(channel, "subscriberCount")
-        self.__video_count = find_value(channel, "videoCount")
-        self.__views_count = find_value(channel, "viewCount")
+    @channel_summary_views.setter
+    def channel_summary_views(self, channel_summary_views: int):
+        self.__channel_summary_views = channel_summary_views
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-
-        # получаем данные о канале по его id
-        channel = self.get_info()
-
-        # выводит словарь в json-подобном удобном формате с отступами
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        pprint(self.channel, sort_dicts=False)
 
     @classmethod
-    def get_service(cls) -> Resource:
-        """Создаёт специальный объект для работы с API"""
-
-        # API_KEY скопирован из гугла и вставлен в переменные окружения
-        api_key: str = os.getenv(cls.name_key)
-
-        manager = build(cls.service, cls.version, developerKey=api_key)
-        return manager
-
-    def get_attributes_dict(self) -> dict:
-        """Возвращает словарь со свойствами класса и их значениями"""
-
-        return {"channel_id": self.channel_id,
-                "title": self.title,
-                "description": self.description,
-                "url": self.url,
-                "subscribers_count": self.subscribers_count,
-                "video_count": self.video_count,
-                "views_count": self.views_count
-                }
+    def get_service(cls):
+        return cls.service
 
     def to_json(self, file_name: str) -> None:
-        """Записывает аттрибуты экземпляра класса в json-файл"""
+        channel_data = {"channel_id": self.__channel_id,
+                        "channel_name": self.__channel_name,
+                        "channel_description": self.__channel_description,
+                        "channel_url": self.__channel_url,
+                        "subscribers_count": self.__channel_subscribers_count,
+                        "video_count": self.__channel_video_count,
+                        "summary_views": self.__channel_summary_views
+                        }
 
-        from_root = "homework-2", file_name
-        to_root = os.path.dirname(os.path.dirname(__file__))
-        path = os.path.join(to_root, *from_root)
-        attributes = self.get_attributes_dict()
-
-        with open(path, "w", encoding="UTF-8") as json_file:
-            json.dump(attributes, json_file, indent=4, separators=(',', ': '), ensure_ascii=False)
-
-    def __add__(self, other) -> int | Exception:
-        """
-        Реализует возможность сложения объектов данного класса
-        между собой по количеству подписчиков
-        """
-
-        if isinstance(other, Channel):
-            return int(self.subscribers_count) + int(other.subscribers_count)
-        else:
-            raise TypeError("Операции между этими объектами невозможны")
-
-    def __sub__(self, other) -> int | Exception:
-        """
-        Реализует возможность вычитания объектов данного класса
-        между собой по количеству подписчиков
-        """
-
-        if isinstance(other, Channel):
-            return int(self.subscribers_count) - int(other.subscribers_count)
-        else:
-            raise TypeError("Операции между этими объектами невозможны")
-
-    def __eq__(self, other) -> bool | Exception:
-        """
-        Реализует возможность сравнения по знаку "==" объектов данного класса
-        между собой по количеству подписчиков
-        """
-
-        if isinstance(other, Channel):
-            return self.subscribers_count == other.subscribers_count
-        else:
-            raise TypeError("Операции между этими объектами невозможны")
-
-    def __lt__(self, other) -> bool | Exception:
-        """
-        Реализует возможность сравнения по знаку "<" объектов данного класса
-        между собой по количеству подписчиков
-        """
-
-        if isinstance(other, Channel):
-            return self.subscribers_count < other.subscribers_count
-        else:
-            raise TypeError("Операции между этими объектами невозможны")
+        with open(file_name, "w", encoding="utf-8") as json_file:
+            json.dump(channel_data, json_file, indent=2, ensure_ascii=False)
