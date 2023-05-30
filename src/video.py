08b0df3e4c8e540af5f7f1e85eb28641a72ec11a
MaxSlidzen <slidzon94@gmail.com>
2023-05-30 19:42:52 +0300

    edited with constants

--- src/video.py
+++ src/video.py
@@ -1,9 +1,4 @@
-import os
-from googleapiclient.discovery import build
-
-
-API_KEY: str = os.getenv('YT_API_KEY')
-YOUTUBE = build('youtube', 'v3', developerKey=API_KEY)
+from src.constants import YOUTUBE
 
 
 class Video:
