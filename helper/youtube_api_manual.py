8c876d55fb3c4b74621a957c1263f955a87eed54
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-30 02:07:46 +0200

    added classes Video and PLVideo (#10)

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -90,7 +90,7 @@ video_id = 'gaoc9MPZ4bw'
 video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                        id=video_id
                                        ).execute()
-# printj(video_response)
+printj(video_response)
 video_title: str = video_response['items'][0]['snippet']['title']
 view_count: int = video_response['items'][0]['statistics']['viewCount']
 like_count: int = video_response['items'][0]['statistics']['likeCount']
