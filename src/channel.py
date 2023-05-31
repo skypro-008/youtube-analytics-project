a785e098509991cf17df72806547846de4d2ffbb
Evgeny <vajnbiy@gmail.com>
2023-05-31 12:42:06 -0700

    corrected

--- src/channel.py
+++ src/channel.py
@@ -13,15 +13,15 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         #youtube = build('youtube', 'v3', developerKey=self.api_key)
         input_dict = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         pprint(input_dict)
         self.title = input_dict['items'][0]['snippet']['title']
         self.description = input_dict['items'][0]['snippet']['description']
-        self.channel_link = input_dict['kind']
+        self.channel_url = input_dict['items'][0]['snippet']['customUrl']
         self.subscribers = input_dict['items'][0]['statistics']['subscriberCount']
-        self.video = input_dict['items'][0]['statistics']['videoCount']
+        self.video_count = input_dict['items'][0]['statistics']['videoCount']
         self.view = input_dict['items'][0]['statistics']['viewCount']
 
 
@@ -33,6 +33,10 @@ class Channel:
     @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=cls.api_key)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
 
     def to_json(self, path):
