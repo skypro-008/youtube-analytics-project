0a72f78e05e731d18e509d6df06ae18a631a49ea
MaxSlidzen <slidzon94@gmail.com>
2023-05-31 03:15:54 +0300

    edited code and add show_best_video

--- src/playlist.py
+++ src/playlist.py
@@ -1,7 +1,6 @@
 from src.constants import YOUTUBE
 from datetime import timedelta
-import isodate
-# from src.video import Video
+from src.video import Video
 
 
 class PlayList:
@@ -15,21 +14,21 @@ class PlayList:
                                                              ).execute()
         self.title = self.playlist['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
-        self.videos = YOUTUBE.videos().list(part='contentDetails',
-                                            id=','.join(video_ids)
-                                            ).execute()
+        self.videos = [Video(video['contentDetails']['videoId'])
+                       for video in self.playlist_videos['items']]
 
 
     @property
     def total_duration(self):
-
         total_duration = timedelta(0)
-        for video in self.videos['items']:
-            video_duration = isodate.parse_duration(video['contentDetails']['duration'])
-            total_duration += video_duration
+        for video in self.videos:
+            total_duration += video.duration
         return total_duration
 
 
-    # def show_best_video(self):
-
+    def show_best_video(self):
+        best_video = self.videos[0]
+        for video in self.videos:
+            if video > best_video:
+                best_video = video
+        return best_video.url
