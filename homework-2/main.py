d92d8be1f3329b4695570d1e562b2dd3a2fb526e
Kirill Aliyev <kirill.linas@gmail.com>
2023-06-01 18:16:29 +0400

    homework

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,15 +2,17 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
 
     # получаем значения атрибутов
-    print(moscowpython.title)  # MoscowPython
-    print(moscowpython.video_count)  # 685 (может уже больше)
-    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+    print(moscowpython.channel_name)  # MoscowPython
+    print(moscowpython.channel_video_count)  # 685 (может уже больше)
+    print(moscowpython.channel_url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    # moscowpython.channel_id = 'Новое название'
+    try:
+        moscowpython.channel_id = 'Новое название'
+    except AttributeError:
+        print("Атрибут channel_id не имеет сеттера!")
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
--- homework-3/main.py
+++ homework-3/main.py
@@ -15,4 +15,3 @@ if __name__ == '__main__':
     print(moscowpython < highload)  # True
     print(moscowpython <= highload)  # True
     print(moscowpython == highload)  # False
-    
