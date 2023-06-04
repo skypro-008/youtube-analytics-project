cc8c6f3f97a2da96c1f06f01da21460ec9553915
User <eco-64@mail.ru>
2023-06-04 21:32:29 +0500

    task is fixed

--- homework-2/main.py
+++ homework-2/main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     print(moscowpython.video_count)  # 685 (может уже больше)
     print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
-    # менять не можем
-    moscowpython.channel_id = 'Новое название'
-    # AttributeError: property 'channel_id' of 'Channel' object has no setter
-
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+
+    # менять не можем
+    moscowpython.channel_id = 'Новое название'
+    # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # создаем файл 'moscowpython.json' в данными по каналу
     moscowpython.to_json('moscowpython.json')
