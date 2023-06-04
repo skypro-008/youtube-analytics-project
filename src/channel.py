a8610c7992ac592cfb702e01d141d7ab92587458
DimaSouthTree <DITsindrin@gmail.com>
2023-06-04 21:09:26 +0300

    added branch develop

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,20 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+import isodate
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('API_KEY_YOUTUBE')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
         pass
 
     def print_info(self) -> None:
