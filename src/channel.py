74b97d71aa3113b61ca4e175c11479b5aaa5a175
DimaSouthTree <DITsindrin@gmail.com>
2023-06-05 00:18:35 +0300

    added branch develop

--- src/channel.py
+++ src/channel.py
@@ -1,8 +1,6 @@
 import json
 import os
-
 from googleapiclient.discovery import build
-
 import isodate
 
 
@@ -10,13 +8,19 @@ class Channel:
     """Класс для ютуб-канала"""
 
     api_key: str = os.getenv('API_KEY_YOUTUBE')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        pass
+
+    def get_service(self):
+        servise = build('youtube', 'v3', developerKey=Channel.api_key)
+        return servise
+
+    def get_channel_info(self):
+        r = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return json.dumps(r)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.get_channel_info())
