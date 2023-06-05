1c8a3ec95f1bf1326dfa6017db755192c6e5d6ef
Renkos Alexander <a.a.renkos@gmail.com>
2023-06-05 14:57:36 +0700

    Homework_6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -8,11 +9,36 @@ class Video:
 
     def __init__(self, video_id) -> None:
         self.video_id = video_id
-        self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.video_url = f'https://youtu.be/{self.video_id}'
-        self.view_count = self.video_info['items'][0]['statistics']['viewCount']
-        self.likes_count = self.video_info['items'][0]['statistics']['likeCount']
+        self.video_id = video_id
+
+        try:
+            self.url = f'https://youtu.be/{self.video_id}'
+
+        except HttpError:
+            self.video_info = None
+            self.title = None
+            self.url = None
+            self.views = None
+            self.like_count = None
+
+        try:
+            self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+
+        except IndexError:
+            self.video_info = None
+            self.title = None
+            self.url = None
+            self.views = None
+            self.like_count = None
+
+        else:
+            self.video_id = video_id
+            self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.video_id}'
+            self.views = self.video_info['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_info['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return self.title
