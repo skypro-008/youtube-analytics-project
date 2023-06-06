9ccaed677fedfe98908b896dbff9d8c3c5e0c4fd
hoolywant <sir.saltickow@yandex.ru>
2023-06-06 22:53:20 +0300

    task 3 completed successfully

--- src/channel.py
+++ src/channel.py
@@ -5,7 +5,7 @@ import json
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = 'AIzaSyBy1jlB4KQiic3Y0RTEgfYJ4rCQvNFZCi4' #os.getenv('YT_API_KEY')
+    api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
