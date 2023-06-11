002c63af8094bfbfa118e9d2aaad0aaf58c6a005
polexa <polexa84@gmail.com>
2023-06-12 01:39:33 +0300

    при инициализации экземпляра класса Video пользователь может передать несуществующий id видео. Внедрил в код блоки try/except , чтобы обработать такую ситуацию.
    Если пользователь передал id, с которым невозможно получить данные о видео по API, то у экземпляра инициализируется только свойство video_id, а остальные поля принимают значение None.

--- src/video.py
+++ src/video.py
@@ -1,27 +1,26 @@
-import os
-from googleapiclient.discovery import build
+from src.playlist import YTMixin
 
 
-class Video:
+class Video(YTMixin):
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    @classmethod
-    def get_service(cls):
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        return youtube
 
     def __init__(self, video_id):
         self.video_id = video_id
-        video_response = Video.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                           id=video_id
-                                                           ).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        try:
+
+            video_response = Video.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                               id=video_id
+                                                               ).execute()
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+        except IndexError:
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return f'{self.video_title}'
