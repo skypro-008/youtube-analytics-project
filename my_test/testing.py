e2037c5421285927015c83095e9aa562e1550896
djkeysi <evgeniyzenchenko@gmail.com>
2023-06-19 20:26:45 +0700

    end_dz_13_2_2_1

--- my_test/testing.py
+++ my_test/testing.py
@@ -1,17 +1,54 @@
 import json
 class Item:
-    def __init__(self,itemall):
+
+    all=[]
+    def __init__(self,itemall,name1):
         self.gg = "ddfdfdfdf"
         self.itemall = itemall
+        self.__name1 = name1
+        self.__all =['Cmartphon',"dffdfdf"]
+        print(self.__all)
 
-    def to_json(self):
-        dict = {
-            "gg": self.gg
-        }
-        with open("moscowpython.json", "w") as file_json:
-            json.dump(dict, file_json)
+    @property
+    def name(self):
+        return self.__all
 
-it = Item("111")
-it.to_json()
+    @name.setter
+    def name(self, value):
+        self.__all = ["value"]
+
+    @property
+    def name1(self):
+        return self.__name
+
+    @name1.setter
+    def name1(self, value):
+        if len(value) >= 10:
+            # print("Длина наименования товара превышает 10 символов")
+            print(value[:10])
+        else:
+            self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.append("str")
+
+        return cls.all
+
+
+
+
+it = Item("111","Cmartphon")
+it.name1 = 'СуперСмартфон'
+Item.instantiate_from_csv()
+
+Item.all.append("hgf123")
+print(Item.all)
+item1 = Item.all[0]
+print(item1[0].name)
+
+
+
+
 
 
