0abf232a5058a543c03f718788e8e8a05bd708ab
Nadezhda Ugay <777ugay@gmail.com>
2023-06-21 20:09:58 +0300

    homework2: add functions to channel.py

--- src/channel.py
+++ src/channel.py
@@ -4,7 +4,6 @@ import os
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YOUTUBE_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -15,9 +14,43 @@ class Channel:
         Дальше все данные будут подтягиваться по API."""
 
         self.id = channel_id
+        self.object = self.get_service()
+        self.info = self.object.channels().list(id=self.id, part='snippet,statistics').execute()
+        self.title = self.info['items'][0]['snippet']['title']
+        self.description = self.info['items'][0]['snippet']['description']
+        self.url = os.path.join('https://www.youtube.com/channel/', self.id)
+        self.subscribers = self.info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.info['items'][0]['statistics']['videoCount']
+        self.views = self.info['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        channel = youtube.channels().list(id=self.id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.info, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращает объект для работы с YouTube API."""
+
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, file):
+        """Сохраняет/добавляет в файл значения атрибутов экземпляра Channel."""
+
+        data = {"id канала": self.id,
+                "название канала": self.title,
+                "описание канала": self.description.strip(),
+                "ссылка на канал": self.url,
+                "количество подписчиков": self.subscribers,
+                "количество видео": self.video_count,
+                "общее количество просмотров": self.views
+                }
+        with open(file, 'a', encoding='utf-8') as f:
+            if os.stat(file).st_size == 0:
+                json.dump([data], f, ensure_ascii=False)
+            else:
+                with open(file, 'r', encoding='utf-8') as f:
+                    channels_list = json.load(f)
+                    channels_list.append(data)
+                with open(file, 'w', encoding='utf-8') as f:
+                    json.dump(channels_list, f, ensure_ascii=False)
