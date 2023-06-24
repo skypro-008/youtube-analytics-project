9aef9fb2d8fde5815a5b87c88538e9bab831a30e
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-24 20:09:08 +0300

    add printj add __init__, print_info for class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,28 @@
+import os
+import json
 
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('API_KEY_YOUTUBE')
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id_youtube = self.channel_id
+        channel = youtube.channels().list(id=channel_id_youtube, part='snippet,statistics').execute()
+        printj(channel)
+
+
+
