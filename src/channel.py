3c8a588096fa5572f21cd146b81e9d74d5a8e296
vink77 <vink.av@mail.ru>
2023-06-24 20:55:24 +0700

    add print

--- src/channel.py
+++ src/channel.py
@@ -15,22 +15,22 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
         api_key: str = os.environ.get("API_YUTUBE_KEY")
-
         # создать специальный объект для работы с API
         youtube = build('youtube', 'v3', developerKey=api_key)
-        print(api_key)
-        print(youtube)
 
-        channel_id = 'UCYp3rk70ACGXQ4gFAiMr1SQ'  # HighLoad Channel
+        channel_id = self.channel_id  # HighLoad Channel
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         print(channel)
-        intr = json.load(channel)
-        intr = channel["items"]
-        print(intr)
-        intro = intr["kind"]
-        print(type(intro))
-        print(intro)
+
+        intro = channel["items"][0]
+        print(f'\nНазвание канала - {intro["snippet"]["title"]}')
+        print(f'Описание канала - {intro["snippet"]["description"]}')
+        print(f'Дата публикации канала - {intro["snippet"]["publishedAt"][:10]}')
+        print(f'Количество просмотров - {intro["statistics"]["viewCount"]}  человек')
+        print(f'Количество подписок - {intro["statistics"]["subscriberCount"]}  человек')
+        print(f'Количество роликов на канале - {intro["statistics"]["videoCount"]}  человек')
+
+
 
