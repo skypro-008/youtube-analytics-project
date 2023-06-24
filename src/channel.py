bfc6726d47022daba57371e6cbe26b67f56a5c10
Vink77 <vink.av@mail.ru>
2023-06-24 17:08:29 +0700

    add hw13

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import os
+from googleapiclient.discovery import build
+import isodate
+import json
 
 
 class Channel:
@@ -5,8 +9,28 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.base_url = 'https://www.googleapis.com/youtube/v3/channels'
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        api_key: str = os.environ.get("API_YUTUBE_KEY")
+
+        # создать специальный объект для работы с API
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        print(api_key)
+        print(youtube)
+
+        channel_id = 'UCYp3rk70ACGXQ4gFAiMr1SQ'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(channel)
+        intr = json.load(channel)
+        intr = channel["items"]
+        print(intr)
+        intro = intr["kind"]
+        print(type(intro))
+        print(intro)
+
