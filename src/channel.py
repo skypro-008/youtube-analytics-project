6b3bc209a12f75eecc5ebb2b3866678e0397eb65
Grandic <d.bitkov@gmail.com>
2023-06-25 16:33:04 +0300

    Add new methods for Channel

--- src/channel.py
+++ src/channel.py
@@ -1,26 +1,51 @@
 import json
 import os
 from googleapiclient.discovery import build
-import isodate
 
-#Неработает os.getenv('YT_API_KEY')
-api_key: str = 'AIzaSyBNte6ggcZnydJxTP7W0H8olVEGbfJnMcY'
-youtube = build('youtube', 'v3', developerKey=api_key)
-# channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
+api_key: str = os.getenv('YT_API_KEY')
+
 
 class Channel:
+    all = []
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.all.append(self)
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        for i in channel['items']:
+            self.__channel_id = i["id"]
+            self.title = i["snippet"]["title"]
+            self.description = i["snippet"]["description"]
+            self.url = f"https://www.youtube.com/channel/{i['id']}"
+            self.subscriber_count = i["statistics"]['subscriberCount']
+            self.video_count = i["statistics"]["videoCount"]
+            self.view_count = i["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+        youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return self.printj(channel)
-    #Задел на будующее
-    #def printj(self, dict_to_print) -> None:
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        #print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def to_json(self, name) -> None:
+        test_j = []
+        test_j.append(getattr(self, "channel_id"))
+        test_j.append(getattr(self, "title"))
+        test_j.append(getattr(self, "url"))
+        test_j.append(getattr(self, "description"))
+        test_j.append(getattr(self, "subscriber_count"))
+        test_j.append(getattr(self, "video_count"))
+        test_j.append(getattr(self, "view_count"))
+        with open(name, "w") as write_file:
+            json.dump(test_j, write_file)
+
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
