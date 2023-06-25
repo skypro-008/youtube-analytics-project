d67666701fffbe997807065190212a362160d866
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-25 13:01:13 +0300

    edit channal.py

--- src/channel.py
+++ src/channel.py
@@ -1,14 +1,8 @@
 import os
 import json
-
 from googleapiclient.discovery import build
 
-api_key: str = os.getenv('API_KEY_YOUTUBE')
-
-
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+API_KEY: str = os.getenv('API_KEY_YOUTUBE')
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -16,13 +10,14 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel_id_youtube = self.channel_id
-        channel = youtube.channels().list(id=channel_id_youtube, part='snippet,statistics').execute()
-        printj(channel)
+
+        #перенести в инит
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
 
 
