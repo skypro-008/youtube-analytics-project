f71df1b14aaab369b5ab0255cb54df96e8a8484f
George Astakhov <goha1115@mail.ru>
2023-07-02 18:09:33 +0300

    changes in channel.py

--- src/channel.py
+++ src/channel.py
@@ -7,14 +7,39 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YOUTUBE_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(
+        self.channel = Channel.get_service().channels().list(
             id=channel_id, part='snippet,statistics'
         ).execute()
+
+        self.id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.custom_url = self.channel['items'][0]['snippet']['customUrl']
+        self.url = f'https://www.youtube.com/{self.custom_url}'
+        self.number_of_subscribers = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.total_views = self.channel['items'][0]['statistics']['viewCount']
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, path):
+        my_data = {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'custom_url': self.custom_url,
+            'url': self.url,
+            'number_of_subscribers': self.number_of_subscribers,
+            'video_count': self.video_count,
+            'total_views': self.total_views
+        }
+        with open(path, 'w') as file:
+            json.dump(my_data, file, indent=2, ensure_ascii=False)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
