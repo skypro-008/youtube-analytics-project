a2e8ca4ec9e92541e28b0c9a373fb2463eb2e917
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-02 20:41:34 +0500

    create get_service and modify __init__

--- src/channel.py
+++ src/channel.py
@@ -8,14 +8,16 @@ import isodate
 class Channel:
     """Класс для ютуб-канала"""
 
+    @classmethod
+    def get_service(cls):
+        """Возвращает специальный объект для работы с ютуб-API"""
+        return build('youtube', 'v3', developerKey=os.environ['YT_API_KEY'])
+
     def __init__(self, channel_id: str):
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """При инициализации создаются атрибуты с основной информацией о канале"""
         self.channel_id = channel_id
-        api_key: str = os.getenv('YT_API_KEY')
-        special_controller_yt = build('youtube', 'v3',
-                                      developerKey=api_key)  # создать специальный объект для работы с API
-        self.channel_summary_info = special_controller_yt.channels().list(id=self.channel_id,
-                                                                          part='snippet,statistics').execute()  # получить информацию о канале
+        self.channel_summary_info = self.get_service().channels().list(id=self.channel_id,
+                                                                       part='snippet,statistics').execute()  # получить информацию о канале
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале в json-подобном удобном формате с отступами"""
