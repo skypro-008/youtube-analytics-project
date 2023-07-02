34db9204deae0053cd7a34360f29f560f769fcdd
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-02 11:43:25 +0500

    add init and print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,46 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+import isodate
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        api_key: str = os.getenv('YT_API_KEY')
+        # создать специальный объект для работы с API
+        special_controller_yt = build('youtube', 'v3', developerKey=api_key)
+        # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        # channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        self.channel_summary_info = special_controller_yt.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        # print(self.channel_summary_info)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.channel_summary_info)
+#
+#     def printj(dict_to_print: dict) -> None:
+#         """Выводит словарь в json-подобном удобном формате с отступами"""
+#         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+#
+test_results = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+test_results.print_info()
+# test_results.printj(test_results.print_info)
+#
+#
+#
+#
+# '''
+# получить данные о канале по его id
+# docs: https://developers.google.com/youtube/v3/docs/channels/list
+#
+# сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
+# '''
+
+
+
+
