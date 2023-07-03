5840e2596e2a570acc09f2a5950e1fd7d6450de9
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-03 21:56:28 +0500

    create property channel_id

--- src/channel.py
+++ src/channel.py
@@ -2,8 +2,6 @@ import json
 import os
 from googleapiclient.discovery import build
 
-import isodate
-
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -13,14 +11,19 @@ class Channel:
         """Возвращает специальный объект для работы с ютуб-API"""
         return build('youtube', 'v3', developerKey=os.environ['YT_API_KEY'])
 
+    @property
+    def channel_id(self) -> str:
+        """Возвращает идентификатор канала"""
+        return self.__channel_id
+
     def __init__(self, channel_id: str):
         """При инициализации создаются атрибуты с основной информацией о канале"""
-        self.channel_id = channel_id
-        self.channel_summary_info = self.get_service().channels().list(id=self.channel_id,
+        self.__channel_id = channel_id
+        self.channel_summary_info = self.get_service().channels().list(id=self.__channel_id,
                                                                        part='snippet,statistics').execute()  # получить информацию о канале
         self.title = self.channel_summary_info['items'][0]['snippet']['title']
         self.description = self.channel_summary_info['items'][0]['snippet']['description']
-        self.url_channel = 'https://www.youtube.com/channel/' + self.channel_id
+        self.url_channel = 'https://www.youtube.com/channel/' + self.__channel_id
         self.subscribers_count = self.channel_summary_info['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel_summary_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_summary_info['items'][0]['statistics']['viewCount']
@@ -32,6 +35,6 @@ class Channel:
     def to_json(self, filename):
         """Сохраняет в файл значения атрибутов экземпляра Channel"""
         data = self.__dict__
-        del(data['channel_summary_info'])
+        del (data['channel_summary_info'])
         with open(filename, 'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=2, ensure_ascii=False)
