db658a28b2f939e1d0d55e3b7b78d42b2f8a5756
Ivan Korekov <Vanek77721@gmail.com>
2023-07-06 13:09:21 +0500

    homework 1

--- src/channel.py
+++ src/channel.py
@@ -13,6 +13,7 @@ class Channel:
 
     youtube = build('youtube', 'v3', developerKey = api_key)
 
+
     def __init__(self, channel_id: str) -> None:
         """
         Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
@@ -26,6 +27,7 @@ class Channel:
         """
         channel = self.youtube.channels().list(id = self.channel_id, part = 'snippet,statistics').execute()
         print(channel)
+
 
     def printj(dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
