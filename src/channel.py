15caef3c20368fb78d7025fae3d33840d0c28861
issaa09 <sahfiyev29@gmail.com>
2023-07-16 00:02:53 +0500

    Initial

--- src/channel.py
+++ src/channel.py
@@ -1,27 +1,72 @@
 import json
 import os
 from googleapiclient.discovery import build
-import isodate
-
-
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        snippet = channel['items'][0]['snippet']
+        statistics = channel['items'][0]['statistics']
 
+        self.__name = snippet['title']
+        self.__description = snippet['description']
+        self.__url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.__subscriber_count = int(statistics['subscriberCount'])
+        self.__video_count = int(statistics['videoCount'])
+        self.__view_count = int(statistics['viewCount'])
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+    @property
+    def channel_id(self) -> str:
+        return self.__channel_id
 
+    @property
+    def title(self) -> str:
+        return self.__name
 
+    @property
+    def description(self) -> str:
+        return self.__description
 
+    @property
+    def url(self) -> str:
+        return self.__url
 
+    @property
+    def subscriber_count(self) -> int:
+        return self.__subscriber_count
+
+    @property
+    def video_count(self) -> int:
+        return self.__video_count
+
+    @property
+    def view_count(self) -> int:
+        return self.__view_count
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API."""
+        return cls.youtube
+
+    def to_json(self, file_path: str) -> None:
+        """Сохраняет значения атрибутов экземпляра Channel в файл в формате JSON."""
+        data = {
+            'channel_id': self.__channel_id,
+            'name': self.__name,
+            'description': self.__description,
+            'url': self.__url,
+            'subscriber_count': self.__subscriber_count,
+            'video_count': self.__video_count,
+            'view_count': self.__view_count
+        }
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
