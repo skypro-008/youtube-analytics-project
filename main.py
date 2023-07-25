07afc0f45da03e16b771a735fafd31eb4c085404
admin <D.Alavaro@gmail.com>
2023-07-25 22:47:47 +0300

    step_4

--- main.py
+++ main.py
@@ -11,13 +11,3 @@ if __name__ == '__main__':
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
-
-
-
-    #print(video1.video_data)
-    # print(video2.video_title)
-    # print(video2.view_count)
-    # print(video2.like_count)
-    # print(video2.comment_count)
-    # print(video2.video_data)
-    print(video2.fetch_playlist_data())
