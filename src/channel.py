5c281493eb749e7e3505ddfa3d06795913f98a3c
TurboYoga <kovin1333@gmail.com>
2023-08-01 23:35:03 +0300

    add def to_json add def get_service add flake8

--- src/channel.py
+++ src/channel.py
@@ -6,14 +6,35 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(
+        self.channel = Channel.get_service().channels().list(
             id=channel_id, part='snippet,statistics'
         ).execute()
+
+        self.id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.custom_url = self.channel['items'][0]['snippet']['customUrl']
+        self.url = f'https://www.youtube.com/{self.custom_url}'
+        self.subscribes_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+
+    def to_json(self, path):
+        data = self.__dict__
+        del data["channel"]
+        with open(path, 'w') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
