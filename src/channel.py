d2db0466ae511a8f7c8bee5ab134f981564ab4ee
Takuro Breath <maxsmurffy@gmail.com>
2023-08-05 18:38:59 +0300

    Implemented the work of the function to show information

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,21 @@
-
+import json
+import os
+from googleapiclient.discovery import build
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key : str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+
