195013666f3cc928770d1607674000efcc3bfb12
TurboYoga <kovin1333@gmail.com>
2023-08-15 21:05:19 +0300

    fix APIMixin

--- src/apimixin.py
+++ src/apimixin.py
@@ -10,5 +10,5 @@ class APIMixin:
     @classmethod
     def get_service(cls):
         """Возвращает объект для работы с API youtube"""
-        service = build('youtube', 'v3', developkey=cls.__API_KEY)
+        service = build('youtube', 'v3', developerKey=cls.__API_KEY)
         return service
