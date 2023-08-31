09ff29efedb487febd5394f1405b6dad7cfbb794
savva.net <savva.net@icloud.com>
2023-09-01 01:44:35 +1000

    HW 13.1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
+import os
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    YT_API_KEY = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        dict_to_print = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
