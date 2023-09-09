8c1eff893352e3fe1e579b32c086a02cae0ec68e
AnnaK <njutcka@yandex.ru>
2023-09-09 16:04:32 +0700

    homework-2

--- src/channel.py
+++ src/channel.py
@@ -12,11 +12,31 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.url = self.channel_data['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscribers_count = self.channel_data['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_data['items'][0]['statistics']['videoCount']
+        self.views_count = self.channel_data['items'][0]['statistics']['viewCount']
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel_data, indent=4, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_name):
+        with open(file_name, 'w') as outfile:
+            json.dump(self.channel_data, outfile)
