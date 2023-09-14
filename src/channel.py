2ca47b4a2f60917edd024efa254730b5e1ebf2ad
denis ukrajev <humanwithbeard@gmail.com>
2023-09-14 20:59:38 +0300

    best!

--- src/channel.py
+++ src/channel.py
@@ -1,18 +1,14 @@
-import json
 from googleapiclient.discovery import build
 
 
 class Channel:
-    def __init__(self, api_key: str, channel_id: str) -> None:
+
+    def __init__(self,api_key: str, channel_id: str) -> None:
         self.api_key = api_key
         self.channel_id = channel_id
-        self.youtube = self.get_service()
-        self.channel_data = self.fetch_channel_data()
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
-    def get_service(self):
-        return build('youtube', 'v3', developerKey=self.api_key)
-
-    def fetch_channel_data(self):
+    def print_info(self) -> None:
         request = self.youtube.channels().list(
             part='snippet,statistics',
             id=self.channel_id
@@ -24,33 +20,13 @@ class Channel:
             snippet = channel_info['snippet']
             statistics = channel_info['statistics']
 
-            channel_data = {
-                'id': self.channel_id,
-                'title': snippet['title'],
-                'description': snippet['description'],
-                'link': f'https://www.youtube.com/channel/{self.channel_id}',
-                'subscriberCount': int(statistics['subscriberCount']),
-                'videoCount': int(statistics['videoCount']),
-                'viewCount': int(statistics['viewCount']),
-            }
-            return channel_data
-        else:
-            return None
-
-    def to_json(self, filename):
-        if self.channel_data:
-            with open(filename, 'w', encoding='utf-8') as file:
-                json.dump(self.channel_data, file, ensure_ascii=False, indent=4)
-
-    def print_info(self):
-        if self.channel_data:
-            print("Channel ID:", self.channel_data['id'])
-            print("Channel Name:", self.channel_data['title'])
-            print("Description:", self.channel_data['description'])
-            print("Link:", self.channel_data['link'])
-            print("Subscriber Count:", self.channel_data['subscriberCount'])
-            print("Video Count:", self.channel_data['videoCount'])
-            print("View Count:", self.channel_data['viewCount'])
+            print("Channel Name:", snippet['title'])
+            print("Description:", snippet['description'])
+            print("Published At:", snippet['publishedAt'])
+            print("Country:", snippet['country'])
+            print("View Count:", statistics['viewCount'])
+            print("Subscriber Count:", statistics['subscriberCount'])
+            print("Video Count:", statistics['videoCount'])
         else:
             print("Channel not found")
 
@@ -60,4 +36,3 @@ if __name__ == '__main__':
     channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
     pythchannel = Channel(api_key, channel_id)
     pythchannel.print_info()
-    pythchannel.to_json('channel_data.json')
