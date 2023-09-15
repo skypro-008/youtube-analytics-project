888623da5a996a71d1fedfe432051a5ebc394bbd
Boris <borik1and@gmail.com>
2023-09-15 11:37:11 +0300

    changing def __init__ and def print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,24 @@
+import os
+from googleapiclient.discovery import build
+# from dotenv import load_dotenv
+# load_dotenv('.env')
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
+        self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         pass
 
     def print_info(self) -> None:
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        playlists = youtube.playlists().list(channelId=self.channel_id,
+                                             part='contentDetails,snippet',
+                                             maxResults=50,
+                                             ).execute()
         """Выводит в консоль информацию о канале."""
-        pass
+        print(playlists)
