70e567a32b004ab07d6ed8fc737c6fcc3170179e
Boris <borik1and@gmail.com>
2023-09-15 12:10:33 +0300

    changing in def __init__

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,8 @@
 import os
+
 from googleapiclient.discovery import build
-# from dotenv import load_dotenv
-# load_dotenv('.env')
+
+os.environ['YT_API_KEY'] = 'AIzaSyBpNdpo_fHPACm0Nopoj45bkRVeAjCpQcc'
 
 
 class Channel:
@@ -10,7 +11,7 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+
 
     def print_info(self) -> None:
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
@@ -21,4 +22,5 @@ class Channel:
                                              maxResults=50,
                                              ).execute()
         """Выводит в консоль информацию о канале."""
+
         print(playlists)
