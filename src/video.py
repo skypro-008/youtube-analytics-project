b1d87ce38646af4e5a2fd7efb1e13677211115d4
git config --global user.email <raperelano@gmail.com>
2023-09-18 11:56:30 +0300

    commit

--- src/video.py
+++ src/video.py
@@ -1,22 +1,32 @@
-from src.baseclass import Apibase
+from src.baseclass import MixinBase
 
-class Video(Apibase):
+
+class Video(MixinBase):
     """Класс для видео с Youtube"""
 
     def __init__(self, video_id: str):
         """Экземпляр инициализируется по id видео. Дальше все данные будут подтягиваться по API."""
-        self.__video_id = video_id
+        try:
+            self.__video_id = video_id
+            self.video_response = (
+                self.get_service().videos().list(id=self.__video_id, part='snippet,statistics')).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url: str = f"https://youtu.be/{self.__video_id}"
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
 
-        self.video_response = (
-            self.get_service().videos().list(id=self.__video_id, part='snippet,statistics').execute())
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url: str = f"https://youtu.be/{self.__video_id}"
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.__video_id = video_id
+            self.title: str = None
+            self.url: str = None
+            self.view_count: int = None
+            self.like_count: int = None
 
-    def __str__(self):
-        """представление класса для пользователя"""
-        return self.video_title
+
+def __str__(self):
+    """представление класса для пользователя"""
+    return self.title
+
 
 class PLVideo(Video):
     """Класс плейлист YOUTUBE"""
