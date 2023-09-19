8e9f13d3f049955ff0b15891052b610ff79690be
Boris <borik1and@gmail.com>
2023-09-19 15:23:50 +0300

    add test Channel

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,18 @@
 from src.channel import Channel
 
 
-def test_print_info():
-    # Создайте экземпляр класса Channel с тестовым channel_id (замените его на реальный)
-    list = []
-    channel_id = 'UCX44TgNXmA_XcBEaeft2elA'
-    channel = Channel(channel_id)
-    # Вызовите метод print_info()
-    channel.print_info()
-    list.append(channel)
-    assert len(list) == 1
+# def test_print_info():
+#     list = []
+#     channel_id = 'UCX44TgNXmA_XcBEaeft2elA'
+#     channel = Channel(channel_id)
+#     # Вызовите метод print_info()
+#     channel.print_info()
+#     list.append(channel)
+#     assert len(list) == 1
 
-    # Добавьте здесь утверждения (assert) для проверки ожидаемых результатов
-    # Например, проверьте, что информация о канале успешно получена
-    # assert len(channel) == 7
+
+def test_Channel():
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    assert moscowpython.title == 'MoscowPython'
+    assert moscowpython.video_count == 707
+    assert moscowpython.url == 'https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A'
