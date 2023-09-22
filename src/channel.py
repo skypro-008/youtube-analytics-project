186a21524d4d8945756bfd3401b9a56c41478193
Acer <dmv053@gmail.com>
2023-09-22 23:22:42 +0400

    homework 3 в классе Channel реализованы методы sts, add, sub, lt, le, gt, ge

--- src/channel.py
+++ src/channel.py
@@ -17,14 +17,34 @@ class Channel:
         self.title = self.info['items'][0]['snippet']['title']
         self.description = self.info['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{channel_id}'
-        self.subscriber_count = self.info['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.info['items'][0]['statistics']['videoCount']
-        self.view_count = self.info['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(self.info['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.info['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.info['items'][0]['statistics']['viewCount'])
 
     @property
     def channel_id(self):
         return self.__channel_id
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
 
     @staticmethod
     def get_service():
