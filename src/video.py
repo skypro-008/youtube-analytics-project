eb132426c3774cf5e829dde70d228e0b0067b732
Acer <dmv053@gmail.com>
2023-09-24 23:09:35 +0400

    homework 4 реализован клас Video (инициализация по id видео) и подкласс PLVideo инициация по id видео и id плейлиста

---
+++ src/video.py
@@ -0,0 +1,34 @@
+from src.channel import Channel
+
+
+class Video:
+    """ класс видео"""
+    youtube = Channel.get_service()
+
+    def __init__(self, video_id: str):
+        """конструктор экземпляра
+        param video_id - id видео ролика"""
+
+        self.info = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id).execute()
+        self.video_id = video_id
+        self.title = self.info['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/watch?v={video_id}'
+        self.view_count = int(self.info['items'][0]['statistics']['viewCount'])
+        self.like_count = int(self.info['items'][0]['statistics']['likeCount'])
+        self.comment_count = int(self.info['items'][0]['statistics']['commentCount'])
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    """Подкласс видео с плейлистом"""
+
+    def __init__(self, video_id, playlist_id):
+        """конструктор экземпляра
+        param video_id - id видео ролика
+        param playlist_id - id плейлиста"""
+
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
