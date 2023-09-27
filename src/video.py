4a4214b66db6b1ab9dd3b99ca34d70e56cbea6f1
savva.net <savva.net@icloud.com>
2023-09-27 20:44:16 +1000

    HW 16.1

--- src/video.py
+++ src/video.py
@@ -6,11 +6,17 @@ class Video(Get_Service):
     def __init__(self, video_id: str):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        VIDEO_INFO = self.youtube.videos().list(id=video_id, part='snippet,statistics').execute()
-        self.title: str = VIDEO_INFO['items'][0]['snippet']['title']
-        self.url: str = f'https://www.youtube.com/watch?v={video_id}'
-        self.view_count: int = VIDEO_INFO['items'][0]['statistics']['viewCount']
-        self.like_count: int = VIDEO_INFO['items'][0]['statistics']['likeCount']
+        try:
+            VIDEO_INFO = self.youtube.videos().list(id=video_id, part='snippet,statistics').execute()
+            self.title: str = VIDEO_INFO['items'][0]['snippet']['title']
+            self.url: str = f'https://www.youtube.com/watch?v={video_id}'
+            self.view_count: int = VIDEO_INFO['items'][0]['statistics']['viewCount']
+            self.like_count: int = VIDEO_INFO['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.title}'
