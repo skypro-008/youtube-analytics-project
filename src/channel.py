b8b460a15c02a5179583e1ad5435a2869e59a1d3
filippov nikita <filippovnr73@gmail.com>
2023-09-28 18:29:23 +0300

    первая дз

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import json
+import os
+from googleapiclient.discovery import build
+import isodate
+
+api_key = 'AIzaSyDoXfDhCmEBqP323Mfo599sILGCvB9-Gb4'
+#пришлось выключить, пока что не подтягивает эту переменную, хотя через консоль ее нахожу
+#api_key: str = os.getenv('YT_API_KEY')
 
 
 class Channel:
@@ -5,8 +13,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
