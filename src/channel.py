24551feb25f4526bbb1e5770e099709ada012b4a
danil <shuvalov58@icloud.com>
2023-10-14 12:20:43 +0300

    update

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,16 @@
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
-
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        API_KEY = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(channel)
