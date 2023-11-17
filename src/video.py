f48b2a7cb24ac847d1b568db492603bf15528b42
TurboYoga <kovin1333@gmail.com>
2023-11-17 15:19:40 +0300

    add try/except for init from api

--- src/video.py
+++ src/video.py
@@ -10,14 +10,21 @@ class Video(APIMixin):
 
     def _init_from_api(self):
         """Получаем данные по API и иницилизируем ими экземпляр класса"""
-        video_response = self.get_service().videos().list(part='snippet,statistics',
-                                                          id=self.__id_video
-                                                          ).execute()
-
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.__id_video}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = self.get_service().videos().list(part='snippet,statistics',
+                                                              id=self.__id_video
+                                                              ).execute()
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.__id_video}'
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except:
+            print('Такого ID не существует')
+        finally:
+            self.video_title: str = None
+            self.url = f'https://youtu.be/{self.__id_video}'
+            self.view_count: int = None
+            self.like_count: int = None
 
 
     def __str__(self):
@@ -30,9 +37,3 @@ class PLVideo(Video):
     def __init__(self, id_video, id_playlist):
         super().__init__(id_video)
         self.id_playlist = id_playlist
-
-
-
-
-
-
