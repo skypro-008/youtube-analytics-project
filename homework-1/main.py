21c532df2e0c71f5f074af218d6070bc1c1f330a
Ivan Zuev <ivan.zuev.1984@mail.ru>
2023-11-23 21:09:16 +0300

    add print

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,7 +2,7 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
+    print(moscowpython.print_info())
 
     """
 {
