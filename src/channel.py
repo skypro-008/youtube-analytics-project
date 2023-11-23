5809ad0136c944182d28c9ea5c613fd91e81075f
Ivan Zuev <ivan.zuev.1984@mail.ru>
2023-11-23 21:09:48 +0300

    add class YouTube

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,25 @@
+import os
+
+from googleapiclient.discovery import build
 
 
-class Channel:
+class YouTube:
+    @classmethod
+    def get_service(cls):
+        api_key = os.getenv('API_Key')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+
+class Channel(YouTube):
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.__id = channel_id
+        channel = self.get_service().channels().list(id=self.__id, part='snippet,statistics').execute()
+        self.ifo = channel
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        return self.ifo
