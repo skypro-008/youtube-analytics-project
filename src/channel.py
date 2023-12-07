a7360b848eb9cdde352062e4fcd2da8aa900e47d
gremv <gremvanek@gmail.com>
2023-12-07 18:24:17 +0500

    ДЗ 2 задание 2 с поправками.

--- src/channel.py
+++ src/channel.py
@@ -15,15 +15,13 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала и другими данными."""
         self.__channel_id = channel_id
-        self.chanel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        self.content = json.dumps(self.chanel, indent=2, ensure_ascii=False)
-        self.file = json.loads(self.content)
-        self.description = self.file['items'][0]['snippet']['description']
-        self.count_subscribers = self.file['items'][0]['statistics']['subscriberCount']
-        self.viewCount = self.file['items'][0]['statistics']['viewCount']
-        self.title = self.file['items'][0]['snippet']['title']
-        self.video_count = self.file['items'][0]['statistics']['videoCount']
-        self.url = f'https://www.youtube.com/channel/{self.file['items'][0]['id']}'
+        self.chanel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.description = self.chanel['items'][0]['snippet']['description']
+        self.count_subscribers = self.chanel['items'][0]['statistics']['subscriberCount']
+        self.view_count = self.chanel['items'][0]['statistics']['viewCount']
+        self.title = self.chanel['items'][0]['snippet']['title']
+        self.video_count = self.chanel['items'][0]['statistics']['videoCount']
+        self.url = f'https://www.youtube.com/channel/{self.chanel['items'][0]['id']}'
 
     @property
     def channel_id(self):
@@ -35,7 +33,8 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        # print(self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        print(self.chanel)
 
     @classmethod
     def get_service(cls):
@@ -48,6 +47,6 @@ class Channel:
                         "url": self.url,
                         "count_subscribers": self.count_subscribers,
                         "video_count": self.video_count,
-                        "viewCount": self.viewCount}
+                        "viewCount": self.view_count}
         with open(param, 'w', encoding='utf-8') as file:
             json.dump(channel_info, file, indent=4, ensure_ascii=False)
