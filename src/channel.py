d1b9dbea34a48414235ae3b2722b1d6eaa3aafc6
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2024-01-03 23:30:34 +0300

    В конструкторе класса инициализированы поля класса и реализован метод print_info() класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
-
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        api_key = 'AIzaSyCWMbqApjkYh1yy_Bk9kiAbUjv1fI9EH0E'
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        request = youtube.channels().list(part='snippet, statistics', id=self.channel_id)
+        response = request.execute()
+
+        print(response)
