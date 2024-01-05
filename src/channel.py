775de48ad56f71d9402c2776532920633d7d7278
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2024-01-06 01:26:35 +0300

    HW2. channel.py: - реализованы новые атрибуты (поля) класса, которые заполняются при создании экземпляра класса. - все поля переведены в защищенный режим - реализованы геттеры защищенных полей

--- src/channel.py
+++ src/channel.py
@@ -8,9 +8,46 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
 
+        info = self.channel_info()['items'][0]
+        self._title = info['snippet']['title']
+        self._description = info['snippet']['description']
+        self._url = f'https://www.youtube.com/channel/{self._channel_id}'
+        self._subscriber_count = info['statistics']['subscriberCount']
+        self._video_count = info['statistics']['videoCount']
+        self._total_views = info['statistics']['viewCount']
+
+    # getters
+    @property
+    def channel_id(self):
+        return self._channel_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def description(self):
+        return self._description
     # classmethods
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def subscriber_count(self):
+        return self._subscriber_count
+
+    @property
+    def video_count(self):
+        return self._video_count
+
+    @property
+    def total_views(self):
+        return self._total_views
+
     @classmethod
     def get_service(cls):
         """
@@ -26,7 +63,7 @@ class Channel:
         Получает информацию о канале через API, путем выполнения запроса к YT Data API.
         :return: Словарь с информацией о канале
         """
-        request = Channel.get_service().channels().list(part='snippet, statistics', id=self.channel_id)
+        request = Channel.get_service().channels().list(part='snippet, statistics', id=self._channel_id)
         response = request.execute()
 
         return response
