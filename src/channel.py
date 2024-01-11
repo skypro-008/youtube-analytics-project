c9f06099054a7f37002a57c60bf4bf09b7f56064
Aleksey_Milyaev <Trane94.milyaevaa@gmail.com>
2024-01-11 09:16:37 +0700

    "fix"

--- src/channel.py
+++ src/channel.py
@@ -5,7 +5,6 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
