0f63eb735549242a50154b9df333c51bfd34bed2
aleksey milyaev <trane94.milyaev@gmail.com>
2024-01-14 09:04:46 +0700

    Homework-3

--- src/channel.py
+++ src/channel.py
@@ -5,7 +5,6 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -13,8 +12,8 @@ class Channel:
         self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscriber = self.channel['items'][0]['statistics']['subscriberCount']
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber = int(self.channel['items'][0]['statistics']['subscriberCount'])
         self.viewCount = self.channel['items'][0]['statistics']['viewCount']
         self.description = self.channel['items'][0]['snippet']['description']
 
@@ -38,3 +37,36 @@ class Channel:
         channel_info["subscriber"] = self.subscriber
         with open(file_name, "w", encoding="utf-8") as file:
             file.write(json.dumps(channel_info, indent=2, ensure_ascii=False))
+
+    def __str__(self):
+        """Магический метод __str__ для вывода названия и ссылки на канал"""
+        return f"{self.title} (https://www.youtube.com/channel/{self.channel_id})"
+    def __add__(self, other):
+        """Магический метод сложения подписчиков"""
+        return self.subscriber + other.subscriber
+
+    def __sub__(self, other):
+        """Магический метод вычитания подписчиков"""
+        return self.subscriber - other.subscriber
+
+    def __gt__(self, other):
+        """Магический метод сравнения больше подписчиков"""
+        return self.subscriber > other.subscriber
+
+    def __ge__(self, other):
+        """Магический метод сравнения больше или равно подписчиков"""
+        return self.subscriber >= other.subscriber
+
+    def __lt__(self, other):
+        """Магический метод сравнения меньше подписчиков"""
+        return self.subscriber < other.subscriber
+
+    def __le__(self, other):
+        """Магический метод сравнения меньше или равно подписчиков"""
+        return self.subscriber <= other.subscriber
+
+    def __eq__(self, other):
+        """Магический метод сравнения равно подписчиков"""
+        return self.subscriber == other.subscriber
+
+###########################################
