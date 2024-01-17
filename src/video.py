6b8719488262456a23f428c85752fb927c21e16c
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2024-01-17 11:36:03 +0100

    создали файл video с классами

---
+++ src/video.py
@@ -0,0 +1,26 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+
+API_KEY = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+
+class Video:
+    def __init__(self, video_id):
+        try:
+            self.video_id = video_id
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.video_title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
