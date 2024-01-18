59a74122f48a3c3925019d589b6c3b154c8fc5fb
nadyash83 <bezborodova@pkkb1.ru>
2024-01-18 22:10:46 +1000

    модифицировать конструктор Channel

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,6 +2,7 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    moscowpython.print_info()
 
     # получаем значения атрибутов
     print(moscowpython.title)  # MoscowPython
--- src/channel.py
+++ src/channel.py
@@ -10,14 +10,32 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.dict = None
         self.channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        youtube = self.get_service()
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.viewCount = channel['items'][0]['statistics']['viewCount']
 
-    def print_info(self) -> None:
+    @staticmethod
+    def print_info() -> None:
         """Выводит в консоль информацию о канале."""
-        self.printj(self.channel)
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
-    def printj(self,dict_to_print: dict) -> None:
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
 
+    def to_json(self, filename):
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(self.dict, file, indent=4, ensure_ascii=False)
