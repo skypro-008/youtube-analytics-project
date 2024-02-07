8407e8566a64730e402fa848126d8581640b0541
mariaonderoz <mskorneeva.87@gmail.com>
2024-02-07 12:05:14 +0700

    added channel title, description, url,subscriber count, video count, view count, classmethod get_service, to_json

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,5 @@
 from googleapiclient.discovery import build
 import os
-import isodate
 from dotenv import load_dotenv
 
 load_dotenv('../.env')
@@ -14,9 +13,45 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        print(self.api_key)
+
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(channel)
+        print(json.dumps(channel, ensure_ascii=False, indent=2))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_channel_id):
+        if new_channel_id:
+            print("AttributeError: property 'channel_id' of 'Channel' object has no setter")
+            self.__channel_id = self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('API-KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_json):
+        json_data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'descrition': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+        with open(file_json, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(json_data, ensure_ascii=False, indent=4))
