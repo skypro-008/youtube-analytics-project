d8255091068900b323a873e9d28af3a1557377fa
Rust <rustem-2013@yandex.ru>
2024-02-11 02:15:22 +0500

    Сделан обработчик ошибок

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,11 @@
 import json
-from googleapiclient.discovery import build
-import isodate
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
 load_dotenv()
+
 # Создаем переменную для API-ключа.
 API_KEY: str = os.getenv('YT_API_KEY')
 
@@ -14,44 +15,40 @@ youtube = build('youtube', 'v3', developerKey=API_KEY)
 # api_key = API_KEY
 # channel_id = "UC-OVMPlMA3-YCIeg4z5z23A"
 
+
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(self.error_handler())
 
-
-# class Channel:
-#     """Class for working with YouTube channel data"""
-#
-#     def __init__(self, api_key: str, channel_id: str) -> None:
-#         self.api_key = api_key
-#         self.channel_id = channel_id
-#         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-#
-#     def get_channel_info(self) -> dict:
-#         """Get information about the YouTube channel"""
-#         channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-#         return channel_data
-#
-#     def get_playlists(self) -> dict:
-#         """Get playlists of the YouTube channel"""
-#         playlists_data = self.youtube.playlists().list(channelId=self.channel_id, part='contentDetails,snippet', maxResults=50).execute()
-#         return playlists_data
-#
-#
-# # Инициализируем объект класса Channel с указанием api_key и channel_id
-# channel = Channel(api_key, channel_id)
-#
-# # Получаем информацию о канале
-# channel_info = channel.get_channel_info()
-# print(channel_info)
-#
-# # Получаем плейлисты канала
-# playlists_info = channel.get_playlists()
+    def error_handler(self):
+        try:
+            channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+            with open('channel_data.json', 'w', encoding='utf-8') as json_file:
+                # Проверка на то, что данные успешно получены и сохранены в файл channel_data.json
+                json.dump(channel, json_file, ensure_ascii=False, indent=2)
+            # Открываем и читаем вновь созданный файл
+            with open('channel_data.json', 'r', encoding='utf-8') as json_file:
+                data_youtube = json.load(json_file)
+                data_youtube = json.dumps(data_youtube, indent=2, ensure_ascii=False)
+            return data_youtube
+        except Exception as e:
+            try:
+                with open('channel_data.json', 'r', encoding='utf-8') as json_file:
+                    data_youtube = json.load(json_file)
+                    data_youtube = json.dumps(data_youtube, indent=2, ensure_ascii=False)
+                    print(f"Произошла ошибка при обращении к YouTube API: {e},\n"
+                          "но данные успешно считаны из файла 'channel_data.json':")
+                    return data_youtube
+            except FileNotFoundError as er:
+                print(f"Произошла ошибка при обращении к YouTube API: {er},\n"
+                      "и не найден локальный файл с данными 'channel_data.json'.")
+            except json.JSONDecodeError as je:
+                print(f"Произошла ошибка при обращении к YouTube API: {je},\n"
+                      "локальный файл 'channel_data.json' найден, но\n"
+                      "произошла ошибка при декодировании данных из файла JSON: {je}")
