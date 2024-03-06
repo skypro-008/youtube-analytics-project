81bbce98437231d56c17b17e6f1431eb7513b62e
Alex <modernswk@gmail.com>
2024-03-06 11:26:43 +0300

    hw 4

--- src/channel.py
+++ src/channel.py
@@ -10,7 +10,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
